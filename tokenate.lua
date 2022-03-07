e8120fb98d6cc663e649a06bddf0ab080debb315
DimitriBarronmore <dimitribarronmore@pm.me>
2022-03-07 12:28:59 -0700

    massively refactor core tokenization loop

--- tokenate.lua
+++ tokenate.lua
@@ -18,16 +18,17 @@ inputstream_base.__index = inputstream_base
 
 ---@param x? number The number of characters to peek ahead. Default 1.
 ---@return string | boolean
---Looks ahead within the current line without consuming characters.
+--Looks ahead within the current line without consuming characters and concatenates to the end.
 --Returns nil if there are not enough remaining characters.
-function inputstream_base:peek(x)
+function inputstream_base:peekTo(x)
     if x == nil then x = 1 end
+    local out_pos = {self.current_line, self.current_index}
     local cline_str = self.lines[self.current_line]
     local cline_len = string.len(cline_str)
     if self.current_index + (x-1) > cline_len then
         return nil
     else
-        return string.sub(cline_str, self.current_index, self.current_index + (x-1))
+        return string.sub(cline_str, self.current_index, self.current_index + (x-1)), out_pos
     end
 end
 
@@ -35,14 +36,15 @@ end
 ---@return string | boolean
 --Looks at the character x-1 ahead in the current line.
 --Returns nil if there are not enough remaining characters.
-function inputstream_base:peekAt(x)
+function inputstream_base:peek(x)
     if x == nil then x = 1 end
+    local out_pos = {self.current_line, self.current_index}
     local cline_str = self.lines[self.current_line]
     local cline_len = string.len(cline_str)
     if self.current_index + (x-1) > cline_len then
         return nil
     else
-        return string.sub(cline_str, self.current_index + (x-1), self.current_index + (x-1))
+        return string.sub(cline_str, self.current_index + (x-1), self.current_index + (x-1)), out_pos
     end
 end
 
@@ -50,13 +52,15 @@ end
 ---@return string consumed_characters
 ---@return table position
 --Consumes characters within the current line, removing them from the stream.
-function inputstream_base:consume(x)
-    local out_str = self:peek(x)
-    if out_str then
-        local out_pos = {self.current_line, self.current_index}
-        self.current_index = self.current_index + x
-        return out_str, out_pos
-    end
+function inputstream_base:advance(x)
+    x = x or 1
+    self.current_index = self.current_index + x
+    -- local out_str = self:peek(x)
+    -- if out_str then
+    --     local out_pos = {self.current_line, self.current_index}
+    --     self.current_index = self.current_index + x
+    --     return out_str, out_pos
+    -- end
 end
 
 ---@return boolean
@@ -65,7 +69,7 @@ end
 function inputstream_base:nextLine()
     -- if self.current_line + 1 <= #self.lines then
         self.current_line = self.current_line + 1
-        self.current_index = 0
+        self.current_index = 1
     --     return true
     -- else
     --     return false
@@ -90,7 +94,7 @@ function export.inputstream_from_text(text)
     local inpstr = { lines = {} }
     for line in text:gmatch("[^\n]+\n?") do
         if line:sub(-1) == "\n" then line = line:sub(1,-2) end
-        print(line)
+        -- print(line)
         table.insert(inpstr.lines, line)
     end
     inpstr.current_line = math.min(1, #inpstr.lines)
@@ -162,89 +166,115 @@ function tokenstream_base:create_macro(name, result, complex)
     self.macros[name] = macro
 end
 
+local function skip_to_significant(inpstr)
+    while true do
+        local next_char = inpstr:peek()
+        if next_char == nil then
+            inpstr:nextLine()
+            if inpstr.current_line > #inpstr.lines then
+                return false
+            end
+        elseif next_char == " " then
+            inpstr:advance()
+        else
+            return true
+        end
+    end
+end
+
+local function handle_directive(inpstr, position)
+    print("directive here", position[1], position[2])
+    inpstr:nextLine()
+end
+
+local function handle_macro(tokstr, inpstr, name)
+    -- print("unhandled macro " .. name)
+    local macrodef = tokstr.macros[name]
+    if macrodef.type == "simple" then
+        inpstr:advance(name:len())
+        inpstr:splice(macrodef.result)
+    else
+        print("unhandled complex macro " .. name)
+        inpstr:advance(name:len())
+    end
+                --    if macrodef.type == "simple" then
+                --     --ignore the word; splice in more text
+                --     inpstr:splice(macrodef.result)
+                --     goto continue
+                --    elseif macrodef.type == "complex" then
+                --        --find arguments and use them
+                --        local after_word = position[2]+#name
+                --        print(after_word)
+                --        local s2, e2, in_parens = string.find(this_line, " *(%b())")
+                --        print(position[1], s2)
+                --         if s2 == after_word then
+                --             inpstr:consume(e2-s2)
+                --             print(in_parens)
+                --             in_parens = in_parens:sub(2,-2)
+                --             local args = {}
+                --             -- for arg in in_parens:gmatch("[^|]*[^\\|]|?") do
+                --             for arg in in_parens:gmatch("[^|]*|?") do
+                --                 table.insert(args, arg:match("[^|]*"))
+                --             end
+                --             print("args",table.concat(args, ","))
+                --             print("splicoe", macrodef.result:format(unpack(args)))
+                --             inpstr:splice(macrodef.result:format(unpack(args)))
+                --             goto continue
+                --         else
+                --             --error, no parens found
+                --             print("no parens found for complex macro")
+                --             goto continue
+                --         end
+                --    end
+end
+
 ---@param inpstr lux_inputstream
 ---@param grammar Grammar
 ---@return lux_tokenstream
 function tokenstream_base:tokenate_stream(inpstr, grammar)
-    while inpstr.current_line <= #inpstr.lines do
-        local this_line = inpstr.lines[inpstr.current_line]
-        local next_char, position = inpstr:consume(1)
-
-        --detect start-of-line directives
-        if next_char == "#" and position[2] == 1 then
-            print("directive here", position[1], position[2])
-            ---HANDLE directive
-            -- do
-            -- end
-            inpstr:nextLine()
-            goto continue
+    while true do
+        local status = skip_to_significant(inpstr)
+        if status == false then
+            break
         end
 
-        -- end of line detection
-        if next_char == nil then
-            inpstr:nextLine()
-            goto continue
-        end
-        --ignore spaces
-        if next_char == " " then
-            goto continue
-        end
+        -- local this_line = inpstr.lines[inpstr.current_line]
+        local next_char, position = inpstr:peek(1)
 
-        --test for words
-        do
-            local s, e = string.find(this_line, "[%a_][%a%d_]*", position[2])
-            if s == position[2] then --match
-                -- print("word:" .. next_char .. inpstr:consume(e-s))
-                local name = next_char .. inpstr:consume(e-s)
-                local type = "name"
-                -- check against macros
-                if self.macros[name] then
-                   local macrodef = self.macros[name]
-                   if macrodef.type == "simple" then
-                    --ignore the word; splice in more text
-                    inpstr:splice(macrodef.result)
-                    goto continue
-                   elseif macrodef.type == "complex" then
-                       --find arguments and use them
-                       local after_word = position[2]+#name
-                       print(after_word)
-                       local s2, e2, in_parens = string.find(this_line, " *(%b())")
-                       print(position[1], s2)
-                        if s2 == after_word then
-                            inpstr:consume(e2-s2)
-                            print(in_parens)
-                            in_parens = in_parens:sub(2,-2)
-                            local args = {}
-                            -- for arg in in_parens:gmatch("[^|]*[^\\|]|?") do
-                            for arg in in_parens:gmatch("[^|]*|?") do
-                                table.insert(args, arg:match("[^|]*"))
-                            end
-                            print("args",table.concat(args, ","))
-                            print("splicoe", macrodef.result:format(unpack(args)))
-                            inpstr:splice(macrodef.result:format(unpack(args)))
-                            goto continue
-                        else
-                            --error, no parens found
-                            print("no parens found for complex macro")
-                            goto continue
-                        end
-                   end
+
+        if next_char == "#" and position[2] == 1 then -- Directive
+            handle_directive(inpstr, position)
+
+        elseif next_char:match("[%a_]") then -- Name / Macros / Keyword
+            local pos = 2
+            while true do
+                local char = inpstr:peek(pos)
+                if char == nil then
+                    break
+                elseif not char:match("[%a%d_]") then
+                    break
                 end
+                pos = pos + 1
+            end
+            local name = inpstr:peekTo(pos-1)
+            local type = "name"
+            if self.macros[name] then
+                handle_macro(self, inpstr, name)
+            else
                 if grammar._keywords[name] then
                     type = "keyword"
                 end
                 self:insertToken(type, name, position)
-                goto continue
+                -- print("word " .. name .. "|")
+                inpstr:advance(pos)
             end
+
+        else
+            position = position or {}
+            print(next_char, position[1], position[2])
+            inpstr:advance()
         end
 
-        ::continue::
-        -- if next_char and next_char ~= "" and next_char ~= " " then --testing
-        --     self:insertToken("test", next_char, position)
-        -- end
-        -- print("nextchar", next_char)
-        -- print(position[1],position[2])
-        -- ::continue2::
     end
 end
 
