82e43065906c497fb69dd8c7435219fd36a5a6d2
DimitriBarronmore <d_barronmore@outlook.com>
2025-10-25 23:26:19 -0600

    testin

--- tests/bruh.lua
+++ tests/bruh.lua
@@ -1,105 +1,112 @@
+ local _ENV = _ENV if _VERSION < "Lua 5.2" then 	_ENV = (getfenv and getfenv()) or _G end		 --0
+ local __export = {}  		 --0
 ----
-do --3
-function test ( fizz ) --11
-fizz |= "buzz" --12
-print ( fizz ) --13
-end --14
+do--3
+local function test ( fizz ) --11
+fizz = fizz or ( "buzz" ) --12
+_ENV.print ( fizz ) --13
+end--14
 test ( ) --15
 test ( "h" ) --16
-end --17
+end--17
 local bruh = "h" --20
-tab = { 1 = "apple" , 2 = "peach" , 3 = "orange" , 500 = "durian" , true = false , bruh = "nill" , [ "str" : rep ( 4 ) : rep ( 4 ) ] : sick } --24
-h = ( txt ) -> print ( txt ) --27
-h ( txt ) -> print ( txt ) --29
-local h ( txt ) -> print ( txt ) --31
-tab = { apple : -> print ( "apple" ) } --33
-h => print ( self . whatever ) --35
-local h ( txt ) => print ( txt ) --36
-do --38
-function decorate ( func ) --39
+local tab = { [ 1 ] = "apple" , [ 2 ] = "peach" , [ 3 ] = "orange" , [ 500 ] = "durian" , [ true ] = false , bruh = "nill" , [ ( "str" ) : rep ( 4 ) : rep ( 4 ) ] = _ENV.sick } --24
+local h = function( txt ) return _ENV.print ( txt ) end --27
+h = function( txt ) return _ENV.print ( txt ) end --29
+local h = function( txt ) return _ENV.print ( txt ) end --31
+tab = { apple = function( ) return _ENV.print ( "apple" ) end } --33
+h = function( self ) return _ENV.print ( self . whatever ) end --35
+local h = function( self , txt ) return _ENV.print ( txt ) end --36
+do--38
+local function decorate ( func ) --39
 return function ( ) --40
-print "hallo" --41
+_ENV.print "hallo" --41
 func ( ) --42
 end --43
-end --44
-function decorate2 ( func ) --45
+end--44
+local function decorate2 ( func ) --45
 return function ( ) --46
-print "world" --47
+_ENV.print "world" --47
 func ( ) --48
 end --49
-end --50
-function decorate3 ( func ) --51
+end--50
+local function decorate3 ( func ) --51
 return function ( ) --52
-print ":3" --53
+_ENV.print ":3" --53
 func ( ) --54
 end --55
-end --56
-@ decorate --57
-@ decorate2 --58
-@ decorate3 --59
-local function bruh ( ) print ( "original message" ) end --60
+end--56
+local bruh = --60
+decorate ( --57
+decorate2 ( --58
+decorate3 ( function --59
+( ) --60
+_ENV.print ( "original message" ) --60
+end ) ) ) --60
 bruh ( ) --61
-print ( "---------" ) --69
-@ decorate --78
-global bruh -> print ( bruh ) --79
-print ( "---------" ) --81
-end --90
-do --92
-global b = c --93
-local a , b = c , b --94
+_ENV.print ( "---------" ) --69
+_ENV. bruh = --79
+decorate ( function( ) return --78
+_ENV.print ( bruh ) end ) --79
+_ENV.print ( "---------" ) --81
+end--90
+do--92
+_ENV.b = _ENV.c --93
+local a , b = _ENV.c , _ENV.b --94
 local b --95
-end --96
-do --99
-let b = ( ) -> print ( b ) --100
-end --101
-do --103
-table = { a : 1 , "b" : 2 , 3 : 3 } --104
-func = ( x , y ) -> ( x = x + y ) --107
-pow_x ( x ) -> do --110
-y = x * x --111
-return y --112
-end --113
-end --114
-do --116
+end--96
+do--99
+local b--100
+b = function( ) return _ENV.print ( _ENV.b ) end --100
+end--101
+do--103
+local table = { a = 1 , [ "b" ] = 2 , [ 3 ] = 3 } --104
+local func = function( x , y ) x = x + y end --107
+local pow_x = function( x ) --110
+local y = x * x --111
+return y end --112
+end--114
+do--116
 local object = { name = "h" } --117
-print_name => ( print ( self . name ) ) --118
-object . y = function ( self ) print ( self . y ) end --120
-print ( y ) --122
-end --123
-do --125
-global table = { 1 , 2 , 3 , 4 = "h" } --126
-table . h = true --127
-table = { 2 } --129
-end --130
-do --132
-global b --133
-a , b , c = 1 , 2 , 3 --134
-do --135
+local print_name = function( self ) _ENV.print ( self . name ) end --118
+object . y = function ( self ) --120
+_ENV.print ( self . y ) --120
+end --120
+_ENV.print ( _ENV.y ) --122
+end--123
+do--125
+_ENV.table = { 1 , 2 , 3 , [ 4 ] = "h" } --126
+_ENV.table . h = true --127
+_ENV.table = { 2 } --129
+end--130
+do--132
+local a, c--?
+a , _ENV.b , c = 1 , 2 , 3 --134
+do--135
 local table = { 1 } --136
-end --137
-global name , name2 = 1 , 1 --138
-clear_table ( table ) -> do --139
-for k , _ in pairs ( table ) do --140
+end--137
+_ENV.name, _ENV.name2 = 1 , 1 --138
+local clear_table = function( table ) --139
+for k , _ in _ENV.pairs ( table ) do --140
 table [ k ] = nil --141
-end --142
-end --143
-print ( # table ) --145
-clear_table ( table ) --146
-print ( # table ) --147
-end --149
-do --151
-global i = 0 --152
+end--142
+end--?
+_ENV.print ( # _ENV.table ) --145
+clear_table ( _ENV.table ) --146
+_ENV.print ( # _ENV.table ) --147
+end--149
+do--151
+_ENV.i = 0 --152
 for i = 1 , 3 do --153
-print ( i ) --154
-end --155
-print ( i ) --156
-end --157
-do --158
-global name , val --159
-name , val = 1 , 1 --160
-for name , val in ipairs ( { 1 , 2 , 3 , 4 } ) do --161
-print ( name , val ) --162
-end --163
-print ( name , val ) --164
+_ENV.print ( i ) --154
+end--155
+_ENV.print ( _ENV.i ) --156
+end--157
+do--158
+_ENV.name , _ENV.val = 1 , 1 --160
+for name , val in _ENV.ipairs ( { 1 , 2 , 3 , 4 } ) do --161
+_ENV.print ( name , val ) --162
+end--163
+_ENV.print ( _ENV.name , _ENV.val ) --164
 end--165
 ---
